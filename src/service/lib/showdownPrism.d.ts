1898182127196ed7f526ed91d0b5f07476e1b6cf
Liam Mann <liam@liammann.com>
2017-01-29 19:06:38 +0000

    .gitignore

--- src/service/lib/showdownPrism.d.ts
+++
@@ -1,5 +0,0 @@
-import 'prismjs';
-export declare function showdownPrism(): {
-    type: string;
-    filter: (text: any, converter: any, options: any) => any;
-};
--- src/service/realmark.service.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="es6-promise" />
-import 'rxjs/add/operator/toPromise';
-export declare class RealMarkService {
-    constructor();
-    fromInput(raw: string): string;
-    fromInputPromise(raw: string): Promise<string>;
-    process(markdown: string): string;
-}
