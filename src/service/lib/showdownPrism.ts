37d5676c54489d98faf3453187ee9d09a32876b7
Liam Mann <liam@liammann.com>
2017-04-18 13:46:29 +0100

    fix for multiple code blocks

--- src/service/lib/showdownPrism.ts
+++ src/service/lib/showdownPrism.ts
@@ -17,76 +17,60 @@ export function showdownPrism(){
         type: 'html',
         filter: function (text : any, converter : any, options : any) {
             // find code blocks with a class
-            // <pre><code class=" : start of a code block with a class
-            // ([a-z0-9]+)        : extract language name (result[1])
-            // (.*?)              : extract class name (result[2])
-            // ">                 : end of code tag
-            // ([\s\S]*?)         : extract code (result[3])
-            // </code></pre>      : end of code block
             var regex = /<pre><code class="([a-zA-Z0-9]+) language\-(.*?)">([\s\S]*?)<\/code><\/pre>/g;
-            
-            var result;
             var html = text;
-            while ((result = regex.exec(text)) !== null) {
-                // get the extracted language and code
-                var language : any = result[1];
-                var code : any     = result[3];
-                 // lower case the language so case does not matter
-                language = language.toLowerCase();
-                
-                // decode HTML entities encoded by showdown
-                // the opposite of replacements taken from showdown's _EncodeCode
-                code = code.replace(/&lt;/g,"<");
-                code = code.replace(/&gt;/g,">");
-                code = code.replace(/&amp;/g,"&");
-                
-                // make sure to decode ampersands last otherwise you will double decode < and >
-                // original      : &lt; makes the '<' symbol
-                // encoded       : &amp;lt; makes the '&lt;' symbol
-                //
-                // Wrong:
-                // replace &amp; : &lt; makes the '&lth;' symbol
-                // replace &lt;  : < makes the < symbol
-                //
-                // Correct:
-                // replace &lt;  : &amp;lt; makes the '<' symbol
-                // replace &amp; : &lt; makes the '<' symbol
-                
-                // highlight the code with prism
-                // get the grammar (language supported by prism)
-                var grammar : any = Prism.languages[language];
+            var results = text.match(regex);
+            var regex2 = new RegExp(/<pre><code class="([a-zA-Z0-9]+) language\-(.*?)">([\s\S]*?)<\/code><\/pre>/g, 'i');
+            if(results && results[0]){
+                for (var i = 0; i < results.length; i++) {
+                    var fresult = regex2.exec(results[i]);
 
-                if (!grammar) {
-                    // the given class name is not a language supported by prism
-                    // skip to the next code block
-                    console.log(Prism.languages);
-                    continue;
+                    // get the extracted language and code
+                    var language : any = fresult[1];
+                    var code : any     = fresult[3];
+                     // lower case the language so case does not matter
+                    language = language.toLowerCase();
+
+                    // decode HTML entities encoded by showdown
+                    // the opposite of replacements taken from showdown's _EncodeCode
+                    code = code.replace(/&lt;/g,"<");
+                    code = code.replace(/&gt;/g,">");
+                    code = code.replace(/&amp;/g,"&");
+                    
+                    // make sure to decode ampersands last otherwise you will double decode < and >
+                    // original      : &lt; makes the '<' symbol
+                    // encoded       : &amp;lt; makes the '&lt;' symbol
+                    //
+                    // Wrong:
+                    // replace &amp; : &lt; makes the '&lth;' symbol
+                    // replace &lt;  : < makes the < symbol
+                    //
+                    // Correct:
+                    // replace &lt;  : &amp;lt; makes the '<' symbol
+                    // replace &amp; : &lt; makes the '<' symbol
+                    
+                    // highlight the code with prism
+                    // get the grammar (language supported by prism)
+                    var grammar : any = Prism.languages[language];
+
+                    if (!grammar) {
+                        // the given class name is not a language supported by prism
+                        // skip to the next code block
+                        console.log("NO PrismJS Language",Prism.languages);
+                        continue;
+                    }
+                    
+                    // do the highlighting
+                    var highlightedCode = Prism.highlight(code, grammar, language);
+
+                    // create the new HTML with the highlighted code and language class
+                    // Prism moves the language class from the <code> element to the <pre> element
+                    //  so we will set the class on the <pre> element
+                    var newHTML : String = '<pre class="language-' + language + '"><code class="language-' + language + '">' + highlightedCode + '</code></pre>';
+
+                    html = html.replace(fresult.input, newHTML);
                 }
-                
-                // do the highlighting
-                var highlightedCode = Prism.highlight(code, grammar, language);
-                
-                // create the new HTML with the highlighted code and language class
-                // Prism moves the language class from the <code> element to the <pre> element
-                //  so we will set the class on the <pre> element
-                var newHTML = '<pre class="language-' + language + '"><code class="language-' + language + '">' + highlightedCode + '</code></pre>';
-
-                // replace the old HTML with the new HTML
-                var oldHTML      = result[0];
-                var oldHTMLIndex = result.index;
-                
-                var beforeOldHTML = html.substring(0, oldHTMLIndex);
-                var afterOldHTML  = html.substring(oldHTMLIndex + oldHTML.length);
-                
-                html = beforeOldHTML + newHTML + afterOldHTML;
-                
-                
-                // the next regex search should start after the end of the new HTML
-                var newHTMLIndex = oldHTMLIndex;
-                
-                regex.lastIndex = newHTMLIndex + newHTML.length;
             }
-            
             return html;
         }
     };
